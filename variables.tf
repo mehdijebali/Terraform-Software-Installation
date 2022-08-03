71746e2df070e0b8768d3be606944571f8983a64
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-08-03 12:23:19 +0100

    Update variables

--- variables.tf
+++ variables.tf
@@ -1,27 +1,17 @@
-variable "AWS_ACCESS_KEY" {
-    type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
-}
+variable "AWS_ACCESS_KEY" {}
 
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "Security_Group"{
-    type = list
-    default = ["sg-24076", "sg-90890", "sg-456789"]
+  default = "sg-0fce23046afdb205a"
 }
 
-variable "AMIS" {
-    type = map
-    default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-0352d5a37fb4f603f"
-        us-west-1 = "ami-0f40c8f97004632f9"
-    }
+variable "AMI_ID" {
+  default = "ami-0b0ea68c435eb488d"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
